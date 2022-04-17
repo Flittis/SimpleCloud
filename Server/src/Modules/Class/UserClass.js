6318364b094456326b316748be479d35c93b5fae
Flittis <mega.aang@gmail.com>
2022-04-17 23:56:03 +0200

    Update UserClass

--- Server/src/Modules/Class/UserClass.js
+++ Server/src/Modules/Class/UserClass.js
@@ -1,11 +1,13 @@
 export class UserData {
     _id;
     name;
-    files_limit;
+    space_limit;
+    space_used;
 
     constructor(opt) {
         this._id = opt._id;
         this.name = opt.name;
-        this.files_limit = opt.files_limit;
+        this.space_limit = opt.space_limit;
+        this.space_used = opt.space_used;
     }
 }
